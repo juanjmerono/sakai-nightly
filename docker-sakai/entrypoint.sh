ea553f343efe3006d12930b9d441a881c3e9153f
Juan Jose Meroño Sánchez <jjmerono@um.es>
2017-02-17 09:49:50 +0100

    Fix access to DB Environment.

--- docker-sakai/entrypoint.sh
+++ docker-sakai/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # This creates a sakai.properties file for sakai based on the envrionment
 # Only create a sakai.properties if the values are set.
-if [[ -n "${DB_ENV_MYSQL_DATABASE}" && -n "${DB_ENV_MYSQL_USER}" && -n "${DB_ENV_MYSQL_PASSWORD}" ]]; then
+if [[ -n "${DBMYSQL_ENV_MYSQL_DATABASE}" && -n "${DBMYSQL_ENV_MYSQL_USER}" && -n "${DBMYSQL_ENV_MYSQL_PASSWORD}" ]]; then
 	cat <<EOF  > /usr/local/tomcat/sakai/sakai.properties
 auto.ddl=true
 vendor@org.sakaiproject.db.api.SqlService=mysql
@@ -12,9 +12,9 @@ hibernate.dialect=org.hibernate.dialect.MySQL5InnoDBDialect
 validationQuery@javax.sql.BaseDataSource=select 1 from DUAL
 testOnBorrow@javax.sql.BaseDataSource=false
 defaultTransactionIsolationString@javax.sql.BaseDataSource=TRANSACTION_READ_COMMITTED
-url@javax.sql.BaseDataSource=jdbc:mysql://db:3306/${DB_ENV_MYSQL_DATABASE}?characterEncoding=UTF-8&useServerPrepStmts=false&cachePrepStmts=true&prepStmtCacheSize=4096&prepStmtCacheSqlLimit=4096
-username@javax.sql.BaseDataSource=${DB_ENV_MYSQL_USER}
-password@javax.sql.BaseDataSource=${DB_ENV_MYSQL_PASSWORD}
+url@javax.sql.BaseDataSource=jdbc:mysql://db:3306/${DBMYSQL_ENV_MYSQL_DATABASE}?characterEncoding=UTF-8&useServerPrepStmts=false&cachePrepStmts=true&prepStmtCacheSize=4096&prepStmtCacheSqlLimit=4096
+username@javax.sql.BaseDataSource=${DBMYSQL_ENV_MYSQL_USER}
+password@javax.sql.BaseDataSource=${DBMYSQL_ENV_MYSQL_PASSWORD}
 
 EOF
 fi
